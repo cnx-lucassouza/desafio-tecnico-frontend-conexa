9b379e94e1a354d1a59ba990e8a5ee1b972501d7
​cnx-lucassouza <lucas.souza@conexasaude.com.br>
2022-07-11 11:54:46 -0300

    feat: page atendimentos

---
+++ frontend/src/Pages/Attendances/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "../../Components/Header";
+
+export const Attendances = ({}) => {
+  return (
+    <div>
+      <Header />
+      <h1>Atendimentos</h1>
+    </div>
+  );
+};
