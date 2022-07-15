1b7a38875a4a5581a33eee8c1fdb4093381c4d2c
​cnx-lucassouza <lucas.souza@conexasaude.com.br>
2022-07-15 16:10:20 -0300

    feat: page de atendimento

---
+++ frontend/src/Components/AttendanceList/index.tsx
@@ -0,0 +1,51 @@
+import { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../Button";
+import { NoAttendance } from "../NoAttendance/index";
+import {
+  AttendanceListContainer,
+  AttendanceListItem,
+  AttendanceListCounter,
+} from "./styles";
+import { AttendanceListProps } from "./types";
+
+export const AttendanceList: FC<AttendanceListProps> = ({ attendances }) => {
+  const navigate = useNavigate();
+
+  if (!attendances.length) {
+    return <NoAttendance>Não há nenhuma consulta agendada</NoAttendance>;
+  }
+  function callAttendance() {
+    navigate("waitingroom");
+  }
+
+  return (
+    <AttendanceListContainer>
+      <AttendanceListCounter>
+        {attendances.length} consultas agendadas
+      </AttendanceListCounter>
+
+      {attendances.map(({ id, date, patient }) => {
+        const fullName = `${patient.first_name} ${patient.last_name}`;
+        const formattedDate = new Date(date).toLocaleDateString("pt-BR");
+        const formattedTime = new Date(date).toLocaleTimeString("pt-BR", {
+          hour: "numeric",
+          minute: "numeric",
+        });
+
+        return (
+          <AttendanceListItem key={id}>
+            <div>
+              <p title={fullName}>{fullName}</p>
+              <span>
+                {formattedDate} às {formattedTime}
+              </span>
+            </div>
+
+            <Button onClick={callAttendance}>Atender</Button>
+          </AttendanceListItem>
+        );
+      })}
+    </AttendanceListContainer>
+  );
+};
---
+++ frontend/src/Components/AttendanceList/types.ts
@@ -0,0 +1,5 @@
+import { Attendance } from "../../Pages/Attendances/types";
+
+export interface AttendanceListProps {
+  attendances: Attendance[];
+}
