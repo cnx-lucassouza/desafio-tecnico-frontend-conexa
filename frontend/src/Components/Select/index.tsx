610d8461b9927ee74e63750d92b11998bc662e00
​cnx-lucassouza <lucas.souza@conexasaude.com.br>
2022-07-15 16:27:35 -0300

    feat: criação de componente select

---
+++ frontend/src/Components/Select/index.tsx
@@ -0,0 +1,46 @@
+import { FC, useRef, useEffect } from "react";
+import { useField } from "@unform/core";
+import { ErrorMessage } from "../ErrorMessage";
+import { SelectContainer } from "./styles";
+import { SelectProps } from "./types";
+
+const Select: FC<SelectProps> = ({ name, label, children, ...rest }) => {
+  const selectRef = useRef(null);
+
+  const { fieldName, defaultValue, registerField, error } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      ref: selectRef,
+      name: fieldName,
+      getValue: (ref) => {
+        return ref.current?.value;
+      },
+      setValue: (ref, newValue) => {
+        ref.current.value = newValue;
+      },
+      clearValue: (ref) => {
+        ref.current.value = "";
+      },
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <SelectContainer>
+      <label htmlFor={fieldName}>{label}</label>
+
+      <select
+        id={fieldName}
+        ref={selectRef}
+        defaultValue={defaultValue}
+        {...rest}
+      >
+        {children}
+      </select>
+
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+    </SelectContainer>
+  );
+};
+
+export { Select };
---
+++ frontend/src/Components/Select/types.ts
@@ -0,0 +1,6 @@
+import { SelectHTMLAttributes } from "react";
+
+export interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  name: string;
+  label: string;
+}
