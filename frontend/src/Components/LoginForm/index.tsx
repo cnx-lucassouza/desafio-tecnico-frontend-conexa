311da5a30e049fe4ae3e90d9eac09743b813e8e0
​cnx-lucassouza <lucas.souza@conexasaude.com.br>
2022-07-15 16:16:28 -0300

    fix: ajuste no componente de formulario pra login

--- frontend/src/Components/LoginForm/index.tsx
+++ frontend/src/Components/LoginForm/index.tsx
@@ -1,16 +1,66 @@
-import { FC } from "react";
+import { FC, useRef, useCallback } from "react";
+import * as Yup from "yup";
 import { Input } from "../Input";
-import Button from "../Button";
+import { FormHandles } from "@unform/core";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../Button";
 import { SignInFormContainer } from "./styles";
-import React from "react";
+import { loginDetails } from "../../Hooks/types";
+import getValidationErrors from "../../utils";
+import { useAuth } from "../../Hooks";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
-const SignInForm: FC = () => {
-  const handleSubmit = React.useCallback(() => {
-    console.log("Enviou");
-  }, []);
+const LoginForm: FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = useCallback(
+    async (data: loginDetails) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const validationSchema = Yup.object().shape({
+          email: Yup.string()
+            .required("E-mail obrigatório")
+            .email("Digite um e-mail válido"),
+          password: Yup.string().required("Senha obrigatória"),
+        });
+
+        await validationSchema.validate(data, {
+          abortEarly: false,
+        });
+
+        await login(data);
+
+        navigate("/attendances");
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        toast.error("Usuário ou senha inválido", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
+    },
+    [login, navigate]
+  );
+
   return (
     <>
-      <SignInFormContainer ref={null} onSubmit={handleSubmit}>
+      <SignInFormContainer ref={formRef} onSubmit={handleSubmit}>
         <Input
           type="email"
           name="email"
@@ -23,10 +73,14 @@ const SignInForm: FC = () => {
           label="Senha"
           placeholder="Digite sua senha"
         />
-        <Button>Entrar</Button>
+        <Button type="submit">
+          Entrar
+        </Button>
       </SignInFormContainer>
+
+      <ToastContainer />
     </>
   );
 };
 
-export { SignInForm };
+export { LoginForm };
