850682fa9910345a90c77022fb9c579178d7d2f8
​cnx-lucassouza <lucas.souza@conexasaude.com.br>
2022-07-15 16:28:55 -0300

    fix: FAQLIST chapado

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,20 @@
+export const FAQ_LIST = [
+  {
+    id: 1,
+    question: "Como alterar a data de agendamento?",
+    answer:
+      "Lorem ipsum dolor sit amet. Ea omnis quaerat eum laborum deserunt sed voluptatem error sit impedit suscipit quo natus officiis. Eos omnis quas ut dolorem voluptatem eum quae quibusdam non consequuntur commodi.",
+  },
+  {
+    id: 2,
+    question: "Como cancelar um agendamento?",
+    answer:
+      "Lorem ipsum dolor sit amet. Ea omnis quaerat eum laborum deserunt sed voluptatem error sit impedit suscipit quo natus officiis. Eos omnis quas ut dolorem voluptatem eum quae quibusdam non consequuntur commodi.",
+  },
+  {
+    id: 3,
+    question: "Como alterar informações de um paciente?",
+    answer:
+      "Lorem ipsum dolor sit amet. Ea omnis quaerat eum laborum deserunt sed voluptatem error sit impedit suscipit quo natus officiis. Eos omnis quas ut dolorem voluptatem eum quae quibusdam non consequuntur commodi.",
+  },
+];
