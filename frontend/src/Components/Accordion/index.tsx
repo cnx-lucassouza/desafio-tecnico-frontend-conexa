0596cb8c4934a058987e0c6923afc55f3152bcd0
​cnx-lucassouza <lucas.souza@conexasaude.com.br>
2022-07-15 16:09:29 -0300

    feat: efeito acordião

---
+++ frontend/src/Components/Accordion/index.tsx
@@ -0,0 +1,21 @@
+import { FC, useState } from "react";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+
+import { AccordionContainer } from "./styles";
+import { AccordionProps } from "./types";
+
+const AccordionAction: FC<AccordionProps> = ({ question, answer }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <AccordionContainer isExpanded={isExpanded}>
+      <button type="button" onClick={() => setIsExpanded(!isExpanded)}>
+        <h4>{question}</h4>
+        {isExpanded ? <FiChevronUp /> : <FiChevronDown />}
+      </button>
+      {isExpanded && <p>{answer}</p>}
+    </AccordionContainer>
+  );
+};
+
+export { AccordionAction };
